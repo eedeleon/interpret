8839686387329ea0e1d33d34dd8d8d06ac733c7c
eedeleon <31962564+eedeleon@users.noreply.github.com>
2019-10-24 14:06:25 -0400

    Stop dropping all warnings from console
    This is a proposed PR to avoid warnings being ignored. This null handler drops all warning logs from the interpret namespace. Debug logs will not show up if the null handler is removed.
    
    The default handler ignored all but warning and error logs.

--- python/interpret-core/interpret/__init__.py
+++ python/interpret-core/interpret/__init__.py
@@ -1,10 +1,6 @@
 # Copyright (c) 2019 Microsoft Corporation
 # Distributed under the MIT software license
 
-# Set default logging handler
-import logging
-from logging import NullHandler
-
 # Export functions
 from .version import __version__  # noqa: F401
 from .visual.interactive import (  # noqa: F401
@@ -18,6 +14,4 @@ from .visual.interactive import shutdown_show_server  # noqa: F401
 from .visual.interactive import init_show_server  # noqa: F401
 from .visual.interactive import status_show_server  # noqa: F401
 from .visual.interactive import set_visualize_provider  # noqa: F401
-from .visual.interactive import get_visualize_provider  # noqa: F401
-
-logging.getLogger(__name__).addHandler(NullHandler())
+from .visual.interactive import get_visualize_provider  # noqa: F40
